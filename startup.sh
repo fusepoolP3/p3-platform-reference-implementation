5aa5b2689a718969f6f61e7149d1e32208f4cb32
Pascal Mainini <pascal@mainini.ch>
2015-03-19 21:42:43 +0100

    starting webserver and not backgrounding last task

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+/etc/init.d/lighttpd start
 su p3 -s /usr/bin/java -- -jar /usr/local/lib/marmotta.jar &
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/proxy.jar &
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/proxy.jar
