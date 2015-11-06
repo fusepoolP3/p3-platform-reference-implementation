69a083f700c38d9bb11207763b30a8b2818453a5
Reto Gmür <reto@apache.org>
2015-11-06 11:03:37 +0100

    Using Marmotta LDP root as ldp root (rather than a newly created LDPC

--- marmotta/boot-scripts/02-marmotta-backend.js
+++ marmotta/boot-scripts/02-marmotta-backend.js
@@ -3,4 +3,10 @@
 (function() {
     var host = window.location.hostname;
     P3BackendConfigurator.prototype.serviceHost = host;
+    
+    P3BackendConfigurator.prototype.getLdpRoot = function() {
+        return new Promise(function(resolve, reject) {
+            resolve("http://"+host+":8181/ldp/");
+        });
+    };
 })();
